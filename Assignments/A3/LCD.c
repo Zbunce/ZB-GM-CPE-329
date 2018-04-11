ffd0aa06b346681117a5f9ea82ed27a058b9caaf
Zbunce <32151600+Zbunce@users.noreply.github.com>
2018-04-11 06:46:17 -0700

    V.A3 for real

--- Assignments/A3/LCD.c
+++ Assignments/A3/LCD.c
@@ -1,6 +1,8 @@
 /*
  * LCD.c
  * Code file for LCD control.
+ * Functions designed for external use:
+ * write_char_LCD, clear_LCD, & home_LCD
  *
  * Date: April 10, 2018
  * Authors: Zach Bunce, Garret Maxon
@@ -27,9 +29,11 @@
 //Change the definitions below to change initialization
 #define DISP_SET    0x0F //0x0F Disp ON, Cursor ON, Blink ON
 #define FXN_SET     0x28 //0x28 4-Bit, 2 line, 5x8 font
-#define ENTRY_SET   0x07 //0x06 -> cursor, addr++, disp++
+#define SHIFT_SET   0x10 //0x10 shifts cursor or disp
+#define ENTRY_SET   0x06 //0x06 -> cursor++
 
 void LCD_CMD(unsigned char, unsigned char, int);
+void LCD_CTRL(unsigned char, int);
 
 //Takes in DDRAM address pixel and ASCII character sym
 //0x00...0x0F DDRAM Addresses
@@ -61,19 +65,42 @@ void clear_LCD(int CLK)
 
 //Sets up I/O register direction
 //Runs through LCD setup procedure
+//USE LCD_3.3V_LCD_NHD DATASHEET PROCEDURE
+//Currently only works for 4-bit mode; scared of timings
 void LCD_INIT(int CLK)
 {
-    P3 -> DIR |= RS | RW | EN;
-    P4 -> DIR = 0xFF; //Using all 8 bits in reg so we can be lazy
-    delay_ms(20, CLK);
-    LCD_CMD(FXN_SET, EN, CLK);
-    delay_us(37, CLK);
-    LCD_CMD(DISP_SET, EN, CLK);
-    delay_us(37, CLK);
-    LCD_CMD(DISP_CLR, EN, CLK);
-    delay_ms(2, CLK);
-    LCD_CMD(ENTRY_SET, EN, CLK);
-    delay_us(37, CLK);
+    P3 -> DIR |= RS | RW | EN;  //Sets reg directions
+    P4 -> DIR = 0xFF;           //Using all 8 bits in reg so we can be lazy
+
+    P3 -> OUT &= ~(RS | RW | EN);
+    P4 -> OUT = 0x00;
+    delay_ms(40, CLK);  //Waits for safe power up
+    P4 -> OUT = 0x30;   //Sets wake up command
+    delay_ms(5, CLK);
+
+    LCD_CTRL(EN, CLK);  //Wake up #1
+    P3 -> OUT &= ~EN;
+    delay_us(160, CLK);
+
+    LCD_CTRL(EN, CLK);  //Wake up #2
+    P3 -> OUT &= ~EN;
+    delay_us(160, CLK);
+
+    LCD_CTRL(EN, CLK);  //Wake up #3
+    P3 -> OUT &= ~EN;
+    delay_us(160, CLK);
+
+    P4 -> OUT = 0x20;   //Guess we're awake
+    LCD_CTRL(EN, CLK);
+    P3 -> OUT &= ~EN;
+
+    //The rest sets up LCD settings
+    LCD_CMD(FXN_SET,    EN, CLK);
+    LCD_CMD(SHIFT_SET,  EN, CLK);
+    LCD_CMD(DISP_SET,   EN, CLK);
+    LCD_CMD(ENTRY_SET,  EN, CLK);
+
+    clear_LCD(CLK); //Bill Murray
 }
 
 //Sets control bits to states given in CTRL
@@ -102,5 +129,5 @@ void LCD_CMD(unsigned char CMD, unsigned char CTRL, int CLK)
         delay_us(30, CLK);  //Nominal 10 ns
     }
     P4 -> OUT = 0x00;
-    delay_us(30, CLK); //Nominal 730 ns
+    delay_us(80, CLK); //Nominal 730 ns
 }
