bd648759e7b3aab6d491593abb27f4b34c827e63
Zbunce <32151600+Zbunce@users.noreply.github.com>
2018-04-14 17:32:07 -0700

    V.P1

---
+++ Assignments/P1/main.c
@@ -0,0 +1,169 @@
+/**
+ * main.c
+ *
+ * Links the keypad to the LCD, displaying key pressed
+ * Consecutive key presses overwrite previous entry
+ *
+ * Date: April 13 2018
+ * Authors: Zach Bunce, Garrett Maxon
+ */
+
+#include "msp.h"
+#include "set_DCO.h"
+#include "delays.h"
+#include "LCD.h"
+#include "keypad.h"
+#include "combo.h"
+
+#define LOCKED      1
+#define UNLOCKED    0
+
+//ASCII hex values for keypad characters
+#define K_1     0x31
+#define K_2     0x32
+#define K_3     0x33
+
+#define K_4     0x34
+#define K_5     0x35
+#define K_6     0x36
+
+#define K_7     0x37
+#define K_8     0x38
+#define K_9     0x39
+
+#define K_Ast   0x2A
+#define K_0     0x30
+#define K_Pnd   0x23
+
+#define K_NP    0x10
+
+void delay_ms(int, int);
+void set_DCO(int);
+void LCD_INIT(int);
+void write_char_LCD(uint8_t, uint8_t, int);
+void write_string_LCD(char word[], uint8_t, int);
+void clear_LCD(int);
+void KEYPAD_INIT();
+uint8_t chk_Keypad();
+
+uint8_t chk_Key();
+int chk_Password(int);
+void LCD_Locked(int);
+
+
+void main(void)
+{
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
+
+    int CLK = 480;
+    int lock = LOCKED;
+    int i;
+    uint8_t addr;
+    set_DCO(CLK);
+    LCD_INIT(CLK);
+    KEYPAD_INIT();
+
+    while(lock == LOCKED)
+    {
+        LCD_Locked(CLK);
+        lock = chk_Password(CLK);
+        clear_LCD(CLK);
+    }
+
+    char word[] = "HELLO WORLD";
+    uint8_t len;
+    while(lock == UNLOCKED)
+    {
+        len = strlen(word);
+        for(i = 0x00; i <= 0x4F; i++) {
+            write_char_LCD(K_NP, i-1, CLK);
+            write_string_LCD(word, i, CLK);
+            delay_ms(200, CLK);
+            if ((i > 0x0F) && (i < 0x40)) {
+                i = 0x40;
+            }
+            else if(i == 0x4F) {
+                i = 0x00;
+            }
+        }
+    }
+}
+
+//
+int chk_Password(int CLK)
+{
+    uint8_t key1;
+    uint8_t key2;
+    uint8_t key3;
+    uint8_t key4;
+    uint8_t addr = 0x40;
+
+    key1 = chk_Key();
+    write_char_LCD(key1, addr, CLK);
+    addr++;
+    if (key1 == K_Ast) {
+        clear_LCD(CLK);
+        return LOCKED;
+    }
+    delay_ms(500, CLK);
+
+    key2 = chk_Key();
+    write_char_LCD(key2, addr, CLK);
+    addr++;
+    if (key2 == K_Ast) {
+        clear_LCD(CLK);
+        return LOCKED;
+    }
+    delay_ms(500, CLK);
+
+    key3 = chk_Key();
+    write_char_LCD(key3, addr, CLK);
+    addr++;
+    if (key3 == K_Ast) {
+        clear_LCD(CLK);
+        return LOCKED;
+    }
+    delay_ms(500, CLK);
+
+    key4 = chk_Key();
+    write_char_LCD(key4, addr, CLK);
+    addr++;
+    if (key4 == K_Ast) {
+        clear_LCD(CLK);
+        return LOCKED;
+    }
+    delay_ms(500, CLK);
+
+    if ((key1 == combo1) && (key2 == combo2) && (key3 == combo3) && (key4 == combo4)) {
+        return UNLOCKED;
+    }
+    else {
+        return LOCKED;
+    }
+
+}
+
+uint8_t chk_Key()
+{
+    uint8_t key = 0x10;
+    while(key == 0x10)
+    {
+        key = chk_Keypad();
+    }
+    return key;
+}
+
+void LCD_Locked(int CLK)
+{
+    uint8_t addr;
+    uint8_t letter;
+    char word[] = "LOCKED ENTER KEY";
+    //Writes lock phrase
+    addr = 0x00;
+    write_string_LCD(word, addr, CLK);
+
+    //Sets cursor to bottom row
+    letter = 0x10;
+    addr   = 0x3F;
+    write_char_LCD(letter, addr, CLK);
+}
---
+++ Assignments/P1/set_DCO.c
@@ -0,0 +1,65 @@
+/*
+ *  set_DCO.c
+ *  Code file for the DCO frequency change function.
+ *
+ *  Date: April 6, 2018
+ *  Authors: Zach Bunce, Garret Maxon
+ */
+
+#include "msp.h"
+#define F_1p5_MeHz  15      //Defines various frequency values in almost MHz (10^5)
+#define F_3_MeHz    30      //MeHz labels are used to indicate this
+#define F_6_MeHz    60      //Blame data type truncation
+#define F_12_MeHz   120
+#define F_24_MeHz   240
+#define F_48_MeHz   480
+
+void set_DCO(int freq)
+{
+    CS->KEY = CS_KEY_VAL;                                   //Unlocks CS registers
+    CS->CTL0 = 0;                                           //Clears CTL0 register
+
+    switch (freq)
+    {
+    case F_1p5_MeHz:
+        CS->CTL0 = CS_CTL0_DCORSEL_0;                       //Sets DC0 to 1.5 MHz
+        CS->CTL1 = CS_CTL1_SELA_2 | CS_CTL1_SELS_3 | CS_CTL1_SELM_3; //Sets the clock references
+        break;
+    case F_3_MeHz:
+        CS->CTL0 = CS_CTL0_DCORSEL_1;                       //Sets DC0 to 3 MHz
+        CS->CTL1 = CS_CTL1_SELA_2 | CS_CTL1_SELS_3 | CS_CTL1_SELM_3; //Sets the clock references
+        break;
+    case F_6_MeHz:
+        CS->CTL0 = CS_CTL0_DCORSEL_2;                       //Sets DC0 to 6 MHz
+        CS->CTL1 = CS_CTL1_SELA_2 | CS_CTL1_SELS_3 | CS_CTL1_SELM_3; //Sets the clock references
+        break;
+    case F_12_MeHz:
+        CS->CTL0 = CS_CTL0_DCORSEL_3;                       //Sets DC0 to 12 MHz
+        CS->CTL1 = CS_CTL1_SELA_2 | CS_CTL1_SELS_3 | CS_CTL1_SELM_3; //Sets the clock references
+        break;
+    case F_24_MeHz:
+        CS->CTL0 = CS_CTL0_DCORSEL_4;                       //Sets DC0 to 24 MHz
+        CS->CTL1 = CS_CTL1_SELA_2 | CS_CTL1_SELS_3 | CS_CTL1_SELM_3; //Sets the clock references
+        break;
+    case F_48_MeHz:
+        // Transition to VCORE Level 1: AM0_LDO --> AM1_LDO
+        while ((PCM->CTL1 & PCM_CTL1_PMR_BUSY))
+            ;
+        PCM->CTL0 = PCM_CTL0_KEY_VAL | PCM_CTL0_AMR_1;
+        while ((PCM->CTL1 & PCM_CTL1_PMR_BUSY));
+
+        // Configure Flash wait-state to 1 for both banks 0 & 1
+        FLCTL->BANK0_RDCTL = (FLCTL->BANK0_RDCTL
+                & ~(FLCTL_BANK0_RDCTL_WAIT_MASK)) | FLCTL_BANK0_RDCTL_WAIT_1;
+        FLCTL->BANK1_RDCTL = (FLCTL->BANK0_RDCTL
+                & ~(FLCTL_BANK1_RDCTL_WAIT_MASK)) | FLCTL_BANK1_RDCTL_WAIT_1;
+
+        CS->CTL0 = CS_CTL0_DCORSEL_5;                       //Sets DC0 to 48 MHz
+        CS->CTL1 = CS->CTL1
+                & ~(CS_CTL1_SELM_MASK | CS_CTL1_DIVM_MASK)| CS_CTL1_SELM_3; //Sets MCLK to DCO
+        break;
+    default:
+        break;
+    }
+    CS->KEY = 0;                                        //Locks CS registers
+}
